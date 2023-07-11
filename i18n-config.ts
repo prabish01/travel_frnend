3e5910f90fc0789dd57ef939456beb29bb0e94e8
aarajbhattarai <aarajbhaattarai@gmail.com>
2023-07-11 18:12:57 +0545

    Localization Removed to fix the build error

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,7 @@
 export const i18n = {
     defaultLocale: 'en',
-    locales: ['en', 'de', 'cs'],
+    locales: ['en'],
 } as const;
 
 export type Locale = typeof i18n['locales'][number];
+// locales: ['en', 'de', 'cs'],
